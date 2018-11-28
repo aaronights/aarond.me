2ff4c2decc15a8cd291f991ec49bed5a0454f044
Aaron Davies <aaronights@gmail.com>
2018-11-29 00:35:07 +1300

    Another name

--- build.js
+++ build.js
@@ -7,9 +7,9 @@ catch(err){
     console.log(err)
 }
 var build = child_process.execSync('gridsome build');
-build.stdout.on('data', function(data) {
+/*build.stdout.on('data', function(data) {
     console.log(data)
-})
+})*/
 try {
     fs.renameSync('./dist/', './docs/');
 }
