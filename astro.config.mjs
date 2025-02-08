e6ae2d7d9b7d1348d7cd1f10fe8ac022a265f15b
Mark Honeychurch <mark@honeychurch.org>
2025-02-09 01:54:58 +1300

    Better sized YouTube video embeds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import embeds from 'astro-embed/integration';
+// import embeds from 'astro-embed/integration';
 import pagefind from "astro-pagefind";
 import robotsTxt from 'astro-robots-txt';
-import oembed from 'remark-oembed';
+// import oembed from 'remark-oembed';
+import remarkYoutube from 'remark-youtube';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://aarond.me',
 	// outDir: './docs',
-	integrations: [embeds(), pagefind(), mdx(), sitemap(), robotsTxt()],
+	integrations: [/* embeds(), */ pagefind(), mdx(), sitemap(), robotsTxt()],
 	markdown: {
-    remarkPlugins: [[oembed, { width: 800, height: 450 }]],
+    // remarkPlugins: [[oembed, { width: 800, height: 450 }]],
+		remarkPlugins: [remarkYoutube],
   },
 });
